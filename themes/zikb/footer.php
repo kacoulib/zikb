28914b4591ca379e9296a110727d7571854bdc7b
karim1412 <coulibaly91karim@gmail.com>
2016-02-23 17:24:41 +0100

    header not done yet

--- themes/zikb/footer.php
+++ themes/zikb/footer.php
@@ -7,20 +7,33 @@
 			focus = false;
 
 			burger.addEventListener('click', function(){
-				var intervalId = window.setInterval(animateTo, .1),
-				headHeight = parseInt(header.style.height || header.offsetHeight || window.getComputedStyle(header, null).getPropertyValue('height'));
-				console.log(headHeight)
-				nav.style.opacity += 0; 
+				var defautlTop = (parseInt(nav.style.top)) || parseInt(window.getComputedStyle(nav, null).top),
+				headHeight = parseInt(header.style.height || header.offsetHeight || window.getComputedStyle(header, null).getPropertyValue('height')),
+				intervalId = window.setInterval(animateTo, .1);
 
+				nav.style.opacity += 0; 
+				console.log(defautlTop+' >= '+ top)
 
 				function animateTo(){
 					top = (parseInt(nav.style.top)) || parseInt(window.getComputedStyle(nav, null).top);
-					nav.style.top = (top + 1) + 'px';
-					nav.style.opacity = (parseInt(nav.style.opacity) + 0.5); 
-					if (parseInt(top) >=  headHeight)
-						window.clearInterval(intervalId);
+					if (!focus){
+						if (parseInt(top) >=  headHeight){
+							window.clearInterval(intervalId);
+							focus = true;
+							return;
+						}
+						nav.style.top = (top + 1) + 'px';
+						nav.style.opacity = (parseInt(nav.style.opacity) + 0.5); 
 					
-					console.log(parseInt(top) +' <= '+ headHeight );
+					}else{
+						if (defautlTop >= parseInt(top)){
+							window.clearInterval(intervalId);
+							focus = false;
+							return;
+						}
+						nav.style.top = (top - 1) + 'px';
+						// nav.style.opacity = (parseInt(nav.style.opacity) - 0.5); 
+					}
 				}
 			});
 		})(document.readyState || window.onload)
